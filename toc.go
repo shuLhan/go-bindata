87513f25697d396dd17951eca11985a3d973c035
Frido Koch <info@fridokoch.de>
2015-10-13 14:44:20 +0200

    Use os.ErrNotExists

--- toc.go
+++ toc.go
@@ -109,12 +109,12 @@ func AssetDir(name string) ([]string, error) {
 		for _, p := range pathList {
 			node = node.Children[p]
 			if node == nil {
-				return nil, fmt.Errorf("Asset %%s not found", name)
+				return nil, os.ErrNotExist
 			}
 		}
 	}
 	if node.Func != nil {
-		return nil, fmt.Errorf("Asset %%s not found", name)
+		return nil, os.ErrNotExist
 	}
 	rv := make([]string, 0, len(node.Children))
 	for childName := range node.Children {
@@ -166,7 +166,7 @@ func Asset(name string) ([]byte, error) {
 		}
 		return a.bytes, nil
 	}
-	return nil, fmt.Errorf("Asset %%s not found", name)
+	return nil, os.ErrNotExist
 }
 
 // MustAsset is like Asset but panics when Asset would return an error.
@@ -192,7 +192,7 @@ func AssetInfo(name string) (os.FileInfo, error) {
 		}
 		return a.info, nil
 	}
-	return nil, fmt.Errorf("AssetInfo %%s not found", name)
+	return nil, os.ErrNotExist
 }
 
 // AssetNames returns the names of the assets.
