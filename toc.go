41cb39b77a818b580e54e85751cac239c9cfea59
Frido Koch <info@fridokoch.de>
2015-10-22 15:58:22 +0200

    Use os.PathError for more information regarding the error

--- toc.go
+++ toc.go
@@ -109,12 +109,12 @@ func AssetDir(name string) ([]string, error) {
 		for _, p := range pathList {
 			node = node.Children[p]
 			if node == nil {
-				return nil, os.ErrNotExist
+				return nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
 			}
 		}
 	}
 	if node.Func != nil {
-		return nil, os.ErrNotExist
+		return nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
 	}
 	rv := make([]string, 0, len(node.Children))
 	for childName := range node.Children {
@@ -166,7 +166,7 @@ func Asset(name string) ([]byte, error) {
 		}
 		return a.bytes, nil
 	}
-	return nil, os.ErrNotExist
+	return nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
 }
 
 // MustAsset is like Asset but panics when Asset would return an error.
@@ -192,7 +192,7 @@ func AssetInfo(name string) (os.FileInfo, error) {
 		}
 		return a.info, nil
 	}
-	return nil, os.ErrNotExist
+	return nil, &os.PathError{Op: "open", Path: name, Err: os.ErrNotExist}
 }
 
 // AssetNames returns the names of the assets.
