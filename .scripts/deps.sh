45f81015b9dffc7335dfc86f4b05a96af2aeab92
M. Shulhan <ms@kilabit.info>
2017-09-13 05:49:45 +0700

    Refactoring Makefile
    The new makefile use gometalinter to check source code and generated
    code. `gometalinter` is installed in vendor using `.scripts/deps.sh` that
    read the dependencies version from `vendor.deps`.
    
    No more re-`go get` when running make.
    
    Everytime source or test files updated, the test will be run, and library
    or binary will be rebuild when executing `make`. I hope no more
    pull request submitted without checking local error message first.

---
+++ .scripts/deps.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+DEPS=$1
+
+VENDOR_DIR=${PWD}/vendor
+VENDOR_SRC=${VENDOR_DIR}/src
+VENDOR_BIN=${VENDOR_DIR}/bin
+
+export GOPATH=${VENDOR_DIR}
+
+while read -r SRC DST VER; do
+
+	REPO_DIR="${VENDOR_SRC}/${DST}"
+
+	echo ""
+	echo ">>> Fetch ${SRC} ${VER}"
+	echo "    into ${REPO_DIR}"
+
+	if [ -d ${REPO_DIR} ]; then
+		CUR_VER=`git -C ${REPO_DIR} describe --tags --exact-match 2>/dev/null || git -C ${REPO_DIR} rev-parse --short HEAD`
+
+		if [ "${CUR_VER}" == "${VER}" ]; then
+			echo ">>> Already up to date..."
+			continue
+		fi
+
+		git -C ${REPO_DIR} checkout master
+		git -C ${REPO_DIR} reset --hard HEAD
+		git -C ${REPO_DIR} fetch --tags
+		git -C ${REPO_DIR} pull -q
+	else
+		mkdir -p ${REPO_DIR}
+		git -C ${REPO_DIR} clone ${SRC} .
+	fi
+
+	git -C ${REPO_DIR} checkout -q ${VER}
+
+	echo ">>> Rebuild ..."
+	go get -a ${DST}/...
+
+	if [[ "${DST}" == *gometalinter ]]; then
+		echo ">>> Install gometalinter tools ..."
+		${VENDOR_BIN}/gometalinter --install;
+	fi
+done < ${DEPS};
